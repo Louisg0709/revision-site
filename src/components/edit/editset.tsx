a0659c368d4f6f46e86c47a59f7f281680bc22c8
louis <louisg0709@gmail.com>
2026-07-19 11:29:33 +0100

    Added insert and delete buttons

--- src/components/edit/editset.tsx
+++ src/components/edit/editset.tsx
@@ -11,17 +11,32 @@ export function EditSet(){
     const setData = useContext(SetContext);
 
     const questions  = setData.questions.map((value, index)=>{return(
-        <EditQuestion key={index} question={value} index={index} setQuestion={(newQuestion: Question)=>{
-            const newQuestions = setData.questions.map((q, i)=>{
-                if (i===index){return(newQuestion)}
-                else{return(q)}
-            })
-            setData.setQuestions(newQuestions);
-        }}/>
+        <div key={value.id}>
+            <EditQuestion question={value} index={index} setQuestion={(newQuestion: Question)=>{
+                const newQuestions = setData.questions.map((q, i)=>{
+                    if (i===index){return(newQuestion)}
+                    else{return(q)}
+                })
+                setData.setQuestions(newQuestions);
+            }}/>
+            <div>
+                <button>Insert Question</button>
+                <button onClick={()=>{
+                    if(setData.questions.length > 1){
+                        setData.setQuestions(setData.questions.filter((q)=> q.id!==value.id ))
+                    }
+                }}>Delete Question</button>
+            </div>
+        </div>
     )});
 
     return(
         <div className={styles.container}>
+            <button>Upload Changes</button>
+            <form>
+                <label>Title: </label>
+                <input type="text" defaultValue={setData.setId}/>
+            </form>
             {questions}
         </div>
     )
